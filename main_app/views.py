6d370f36a6726ba3efd40abbd2b877303cf0edfb
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-02-07 15:05:39 -0800

    adds create, update, and delete functionality

--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,45 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
 # Create your views here.
 
-class Finch:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-finches = [
-    Finch('sal', 'Double-Barred Finch',  'has two beaks', 4),
-    Finch('lard', 'The Bengalese Finch',  'can sing', 5),
-    Finch('bobby', 'Star Finch', 'Has a scarlet bill and a yellow belly', 7),
-    Finch('marvin', 'Strawberry Finch',  'Bright Red like the Burning SUN', 2),
-]
-
 def home(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    return render(request,'base.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {'finches': finches })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchCreate(UpdateView):
+    model = Finch
+    fields = ['breed', 'description', 'age']
+
+class FinchCreate(DeleteView):
+    model = Finch
+    success_url = '/finches/'
+    
+
+
+
+
+
+
+
+
+# finches = [
+#     Finch('sal', 'Double-Barred Finch',  'has two beaks', 4),
+#     Finch('lard', 'The Bengalese Finch',  'can sing', 5),
+#     Finch('bobby', 'Star Finch', 'Has a scarlet bill and a yellow belly', 7),
+#     Finch('marvin', 'Strawberry Finch',  'Bright Red like the Burning SUN', 2),
+# ]
